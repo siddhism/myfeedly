464abb0d1e8afb1c6988ee3c42d63bdf6d313c5f
siddhesh suthar <siddhesh.it@gmail.com>
2015-12-18 20:03:22 +0530

    feedfetcher/tests.py

--- feedfetcher/tests.py
+++ feedfetcher/tests.py
@@ -2,5 +2,8 @@ from django.test import TestCase
 
 # Create your tests here.
 class MyTest(TestCase):
-	def firstTest(self):
+	def test_firstTest(self):
 		self.assertEqual( 1+2, 3)
+
+	def test_second(self):
+		self.assertEqual(5-2, 3)
