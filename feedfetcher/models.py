4c633d4ee16dc7a12b223819be0d619e4ecb5510
siddhesh suthar <siddhesh.it@gmail.com>
2015-12-18 20:54:17 +0530

    changed model

--- feedfetcher/models.py
+++ feedfetcher/models.py
@@ -6,3 +6,8 @@ class RssUrl(models.Model):
 	url = models.CharField(max_length=100, unique=True)
 	def __unicode__(self):
 		return smart_unicode(self.url)
+
+class Dummy(models.Model):
+	name = models.CharField(max_length = 100)
+	def __unicode__(self):
+		return smart_unicode(self.name)
